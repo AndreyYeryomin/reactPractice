17763fc7ca00786a7165536f7d9c10cde34aedb9
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2017-11-02 21:40:02 +0200

    validation props through PropTypes

--- react-ractice/src/Header.js
+++ react-ractice/src/Header.js
@@ -1,6 +1,27 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import {Component, } from 'react';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
+    static propTypes ={
+    items: PropTypes.array.isRequired,
+        isLoading: PropTypes.bool,
+        submit: PropTypes.func.isRequired,
+        title: PropTypes.string.isRequired,
+        type : PropTypes.oneOf('news','photos'),
+        user: PropTypes.shape({
+           name: PropTypes.string,
+           age: PropTypes.number
+        }),
+        users :PropTypes.arrayOf(
+            PropTypes.shape({
+            name: PropTypes.string,
+            age: PropTypes.number
+        })
+        )
+
+
+    };
     render() {
         console.log(this.props.items);
         return (
@@ -11,4 +32,5 @@ class Header extends Component {
             </div>);
     }
 }
+
 export default Header;
