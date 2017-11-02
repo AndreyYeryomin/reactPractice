7ad291dc818beaaab6426ac86e8d365ee5bafffc
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2017-11-02 21:02:30 +0200

    local state

---
+++ react-ractice/src/Dropdown.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+class Dropdown extends Component {
+    constructor(props){
+        super(props);
+        this.state = { isOpened:false};
+    }
+    toggleState(){
+        this.setState({ isOpened: !this.state.isOpened})
+    }
+    render() {
+        console.log(this.state.isOpened);
+        let dropdownText;
+        if(this.state.isOpened){
+            dropdownText = <div> Here is what is shown in dropdown</div>
+        }
+        return <div onClick={this.toggleState.bind(this)}>
+            Its DropDown
+            {dropdownText}
+        </div>;
+    }
+}
+export default Dropdown;
